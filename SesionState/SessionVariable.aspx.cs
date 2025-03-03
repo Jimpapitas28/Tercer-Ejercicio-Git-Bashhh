973e30db931cb189ff3f3b79c2c1233a5dadd0e8
Jim z <marlendevasquezz@gmail.com>
2025-03-02 22:10:58 -0600

    Primer commit

---
+++ SesionState/SessionVariable.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SesionState
+{
+	public partial class SessionVariable : System.Web.UI.Page
+	{
+		protected void Page_Load(object sender, EventArgs e)
+		{
+
+		}
+
+        protected void btnCofirm_Click(object sender, EventArgs e)
+        {
+			Session["ddlCategory"] = ddlCategory.SelectedValue;
+            Session["ddSupplier"] = ddlSupplier.SelectedValue;
+			Session["strProduct"] = txtProduct.Text;
+			Session["strImage"] = txtImage.Text;
+			Session["decPrice"] = txtPrice.Text;
+			Session["byNumberInStock"] = txtNumberInStock.Text;
+			Session["bytNumberOnOrder"] = txtNumberOnOrder.Text;
+			Session["bytReorderLevel"] = txtReorderLevel.Text;
+
+
+        }
+    }
+}
---
+++ SesionState/SessionVariable2.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SesionState
+{
+	public partial class SessionVariable2 : System.Web.UI.Page
+	{
+		protected void Page_Load(object sender, EventArgs e)
+		{
+			ddlCategory.SelectedValue = Session["ddlCategory"].ToString();
+			ddlSupplier.SelectedValue = Session["ddlSupplier"].ToString();
+			txtProduct.Text = Session["strProduct"].ToString();
+			txtDescription.Text = Session["strDescription"].ToString();
+			txtImage.Text = Session["strImage"].ToString();
+			Decimal decPrice = Convert.ToDecimal(Session["decPrice"]);
+			txtPrice.Text = decPrice.ToString();
+			txtNumberInStock.Text = Session["bytNumberInStock"].ToString();
+			txtNumberOnOrder.Text = Session["bytNumberOnOrder"].ToString();
+			txtReorderLevel.Text = Session["bytReordrLevel"].ToString();
+			Byte byNumberInStock = Convert.ToByte(Session["byNumberInStock"]);
+			Byte byNumberOnOrder = Convert.ToByte(Session["byNumberOnOrder"]);
+			Decimal decValueInStock = decPrice * byNumberInStock;
+			Decimal decValueOnOrder = decPrice * byNumberOnOrder;
+			txtNumberInStock.Text = decValueInStock.ToString("c");
+			txtNumberOnOrder.Text = decValueOnOrder.ToString("c");
+		}
+	}
+}
